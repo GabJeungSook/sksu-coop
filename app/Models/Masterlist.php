d4972ac1024a0ae537dd773274567da6c20fb907
GabJeungSook <gicawalo@gmail.com>
2023-06-15 13:50:42 +0800

    update image

--- app/Models/Masterlist.php
+++ app/Models/Masterlist.php
@@ -4,9 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Masterlist extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    public function getImagePathAttribute($value){
+        return Storage::url($value);
+    }
 }
