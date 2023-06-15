81318015593ead0f68749ae9a97ddd918a2d2aad
GabJeungSook <gicawalo@gmail.com>
2023-06-15 14:42:01 +0800

    push ah

--- app/Http/Livewire/Admin/Forms/ViewMasterlistData.php
+++ app/Http/Livewire/Admin/Forms/ViewMasterlistData.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Livewire\Admin\Forms;
 
-use App\Models\Masterlist;
-use Livewire\Component;
-use Illuminate\Support\Facades\Storage;
+use Carbon\Carbon;
 use Filament\Forms;
-use Illuminate\Contracts\View\View;
+use Livewire\Component;
+use App\Models\Masterlist;
 use WireUi\Traits\Actions;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Storage;
 
 class ViewMasterlistData extends Component implements Forms\Contracts\HasForms
 {
@@ -15,6 +16,28 @@ class ViewMasterlistData extends Component implements Forms\Contracts\HasForms
     use Actions;
 
     public $record;
+    public $full_name;
+    public $address;
+    public $birthday;
+    public $age;
+    public $tin_number;
+    public $gender;
+    public $civil_status;
+    public $religion;
+    public $dependent_number;
+    public $educational_attainment;
+    public $occupation;
+    public $income;
+    public $date_accepted;
+    public $bod_number;
+    public $membership_type;
+    public $number_of_shares;
+    public $amount_subscribed;
+    public $initial_paid_up;
+    public $bod_resolution;
+    public $date_created;
+
+
     public $file_path =[];
     public $updateImage = false;
     protected $listeners = ['refreshComponent' => '$refresh'];
@@ -27,6 +50,42 @@ class ViewMasterlistData extends Component implements Forms\Contracts\HasForms
             ->disk('public')
             ->preserveFilenames(),
         ];
+    }
+
+    public function mount()
+    {
+        $this->full_name = strtoupper($this->record->first_name.' '.$this->record->middle_name.' '.$this->record->last_name);
+        $this->address = strtoupper($this->record->address);
+        $this->birthday =  Carbon::parse($this->record->date_of_birth)->format('F d, Y');
+        $this->age = $this->record->age;
+        $this->tin_number = $this->record->tin_number;
+        if($this->record->gender === "M")
+        {
+            $this->gender = "Male";
+        }elseif($this->record->gender === "F"){
+            $this->gender = "Female";
+        }
+        if($this->record->civil_status === "M")
+        {
+            $this->civil_status = "Married";
+        }elseif($this->record->gender === "S"){
+            $this->civil_status = "Single";
+        }
+        $this->religion = $this->record->religion;
+        $this->dependent_number = $this->record->dependent_number;
+        $this->educational_attainment = $this->record->educational_attainment;
+        $this->occupation = $this->record->occupation;
+        $this->income = '₱ '.' '.number_format($this->record->income, 2, '.', ',');
+        $this->date_accepted =  Carbon::parse($this->record->date_accepted)->format('F d, Y');
+        $this->bod_number =  $this->record->bod_number;
+        $this->membership_type = strtoupper($this->record->membership_type);
+        $this->number_of_shares =  $this->record->number_of_shares.' Shares';
+        $this->amount_subscribed =  '₱ '.' '.number_format($this->record->amount_subscribed, 2, '.', ',');
+        $this->initial_paid_up =  '₱ '.' '.number_format($this->record->initial_paid_up, 2, '.', ',');
+        $this->bod_resolution =   $this->record->bod_resolution;
+        $this->date_created =  Carbon::parse($this->record->date_created)->format('F d, Y');
+
+
     }
 
     public function redirectToMasterlist()
--- resources/views/livewire/admin/forms/view-masterlist-data.blade.php
+++ resources/views/livewire/admin/forms/view-masterlist-data.blade.php
@@ -10,8 +10,28 @@
     </div>
     <div class="mt-3 overflow-hidden bg-gray-50 shadow sm:rounded-lg">
         <div class="border-t border-gray-100">
-          <dl class="divide-y divide-gray-100">
-            <div class="px-4 py-6 sm:grid sm:grid-cols-5 sm:gap-4 sm:px-6">
+          <dl class=" divide-gray-100">
+            <dt class="p-3 text-sm font-semibold text-gray-900 space-x-3">
+                <span>Member ID:</span>
+                <span>{{$record->member_id}}</span>
+                @if ($record->bod_resolution != null || $record->bod_resolution != '')
+                <span class="inline-flex items-center gap-x-1.5 rounded-md bg-red-100 px-2 py-1 text-xs font-medium text-red-700">
+                    <svg class="h-1.5 w-1.5 fill-red-500" viewBox="0 0 6 6" aria-hidden="true">
+                      <circle cx="3" cy="3" r="3" />
+                    </svg>
+                    Terminated
+                  </span>
+                @else
+                <span class="inline-flex items-center gap-x-1.5 rounded-md bg-green-100 px-2 py-1 text-xs font-medium text-green-700">
+                    <svg class="h-1.5 w-1.5 fill-green-500" viewBox="0 0 6 6" aria-hidden="true">
+                      <circle cx="3" cy="3" r="3" />
+                    </svg>
+                    Active
+                  </span>
+                @endif
+
+            </dt>
+            <div class="px-4 py-6 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-6">
                 @php
                 $gender = '';
                 $civil_status = '';
@@ -48,27 +68,10 @@
                     }
                 @endphp
                 <div class="space-y-3">
-                    <dt class="text-sm font-normal text-gray-900 space-x-3">
-                        <span>Member ID:</span>
-                        <span>{{$record->member_id}}</span>
-                        @if ($record->bod_resolution != null || $record->bod_resolution != '')
-                        <span class="inline-flex items-center gap-x-1.5 rounded-md bg-red-100 px-2 py-1 text-xs font-medium text-red-700">
-                            <svg class="h-1.5 w-1.5 fill-red-500" viewBox="0 0 6 6" aria-hidden="true">
-                              <circle cx="3" cy="3" r="3" />
-                            </svg>
-                            Terminated
-                          </span>
-                        @else
-                        <span class="inline-flex items-center gap-x-1.5 rounded-md bg-green-100 px-2 py-1 text-xs font-medium text-green-700">
-                            <svg class="h-1.5 w-1.5 fill-green-500" viewBox="0 0 6 6" aria-hidden="true">
-                              <circle cx="3" cy="3" r="3" />
-                            </svg>
-                            Active
-                          </span>
-                        @endif
 
-                    </dt>
-                    <dt class="text-sm font-normal text-gray-900 space-x-3"><span>Full Name:</span>  <span class="text-sm">{{strtoupper($record->first_name).' '.strtoupper($record->middle_name).' '.strtoupper($record->last_name)}}</span></dt>
+                    <x-input label="Full Name" disabled placeholder="your name" wire:model="full_name" />
+
+                    {{-- <dt class="text-sm font-normal text-gray-900 space-x-3"><span>Full Name:</span>  <span class="text-sm">{{strtoupper($record->first_name).' '.strtoupper($record->middle_name).' '.strtoupper($record->last_name)}}</span></dt>
                     <dt class="text-sm font-normal text-gray-900 space-x-3"><span>Address:</span>  <span class="text-sm">{{$record->address == null ? '---' : strtoupper($record->address)}}</span></dt>
                     <dt class="text-sm font-normal text-gray-900 space-x-3"><span>Birthday:</span>  <span class="text-sm">{{$record->date_of_birth == null ? '---' : Carbon\Carbon::parse($record->date_of_birth)->format('F d, Y')}}</span></dt>
                     <dt class="text-sm font-normal text-gray-900 space-x-3"><span>Age:</span>  <span class="text-sm">{{$record->age == 0 ? '---' : $record->age}}</span></dt>
@@ -76,10 +79,16 @@
                     <dt class="text-sm font-normal text-gray-900 space-x-3"><span>Gender:</span>  <span class="text-sm">{{strtoupper($gender)}}</span></dt>
                     <dt class="text-sm font-normal text-gray-900 space-x-3"><span>Civil Status:</span>  <span class="text-sm">{{strtoupper($civil_status)}}</span></dt>
                     <dt class="text-sm font-normal text-gray-900 space-x-3"><span>Religion / Social Afilliation:</span class="text-sm">  <span>{{$record->religion == null ? '---' : strtoupper($record->religion)}}</span></dt>
-                    <dt class="text-sm font-normal text-gray-900 space-x-3"><span>Number of Dependent:</span>  <span class="text-sm">{{$record->dependent_number == null ? '---' : $record->dependent_number}}</span></dt>
-
+                    <dt class="text-sm font-normal text-gray-900 space-x-3"><span>Number of Dependent:</span>  <span class="text-sm">{{$record->dependent_number == null ? '---' : $record->dependent_number}}</span></dt> --}}
                 </div>
-
+                <x-input label="Address" disabled placeholder="---" wire:model="address" />
+                <x-input label="Birthday" disabled placeholder="---" wire:model="birthday" />
+                <x-input label="Age" disabled placeholder="---" wire:model="age" />
+                <x-input label="TIN" disabled placeholder="---" wire:model="tin_number" />
+                <x-input label="Gender" disabled placeholder="---" wire:model="gender" />
+                <x-input label="Civil Status" disabled placeholder="---" wire:model="civil_status" />
+                <x-input label="Religion" disabled placeholder="---" wire:model="religion" />
+                <x-input label="Number of Dependent" disabled placeholder="---" wire:model="dependent_number" />
             </div>
             {{-- <div class="px-4 py-6 sm:grid sm:grid-cols-5 sm:gap-4 sm:px-6">
             </div> --}}
@@ -94,13 +103,15 @@
       <div class="mt-3 overflow-hidden bg-gray-50 shadow sm:rounded-lg">
         <div class="border-t border-gray-100">
           <dl class="divide-y divide-gray-100">
-            <div class="px-4 py-6 sm:grid sm:grid-cols-5 sm:gap-4 sm:px-6">
+            <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                 <div class="space-y-3">
-                    <dt class="text-sm font-normal text-gray-900 space-x-3"><span class="text-sm">Highest Educational Attainment:</span>  <span>{{$record->educational_attainment == null ? '---' : $record->educational_attainment}}</span></dt>
+                    <x-input label="Highest Educational Attainment" disabled placeholder="---" wire:model="educational_attainment" />
+                    {{-- <dt class="text-sm font-normal text-gray-900 space-x-3"><span class="text-sm">Highest Educational Attainment:</span>  <span>{{$record->educational_attainment == null ? '---' : $record->educational_attainment}}</span></dt>
                     <dt class="text-sm font-normal text-gray-900 space-x-3"><span class="text-sm">Occupation:</span>  <span>{{$record->occupation == null ? '---' :   $record->occupation}}</span></dt>
-                    <dt class="text-sm font-normal text-gray-900 space-x-3"><span class="text-sm">Annual Income:</span>  <span>₱ {{$record->income == null ? '---' : number_format($record->income, 2, '.', ',')}}</span></dt>
+                    <dt class="text-sm font-normal text-gray-900 space-x-3"><span class="text-sm">Annual Income:</span>  <span>₱ {{$record->income == null ? '---' : number_format($record->income, 2, '.', ',')}}</span></dt> --}}
                 </div>
-
+                <x-input label="Occupation" disabled placeholder="---" wire:model="occupation" />
+                <x-input label="Annual Income" disabled placeholder="---" wire:model="income" />
             </div>
           </dl>
         </div>
@@ -109,20 +120,26 @@
       <div class="mt-3 overflow-hidden bg-gray-50 shadow sm:rounded-lg">
         <div class="border-t border-gray-100">
           <dl class="divide-y divide-gray-100">
-            <div class="px-4 py-6 sm:grid sm:grid-cols-5 sm:gap-4 sm:px-6">
+            <div class="px-4 py-6 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-6">
                 <div class="space-y-3">
-                    <dt class="text-sm font-normal text-gray-900 space-x-3"><span class="text-sm">Date Accepted:</span>  <span>{{$record->date_accepted == null ? '--- ': Carbon\Carbon::parse($record->date_accepted)->format('F d, Y')}}</span></dt>
+                    <x-input label="Date Accepted" disabled placeholder="---" wire:model="date_accepted" />
+
+                    {{-- <dt class="text-sm font-normal text-gray-900 space-x-3"><span class="text-sm">Date Accepted:</span>  <span>{{$record->date_accepted == null ? '--- ': Carbon\Carbon::parse($record->date_accepted)->format('F d, Y')}}</span></dt>
                     <dt class="text-sm font-normal text-gray-900 space-x-3"><span class="text-sm">BOD Resolution Number:</span>  <span>{{$record->bod_number == 0 ? '---' : $record->bod_number}}</span></dt>
                     <dt class="text-sm font-normal text-gray-900 space-x-3"><span class="text-sm">Type / Kind of Membership:</span>  <span>{{$record->membership_type == null ? '---' : strtoupper($record->membership_type)}}</span></dt>
                     <dt class="text-sm font-normal text-gray-900 space-x-3"><span class="text-sm">Number of Shares Subscribed:</span>  <span>{{$record->number_of_shares == null ? '---' : $record->number_of_shares.' Shares'}}</span></dt>
                     <dt class="text-sm font-normal text-gray-900 space-x-3"><span class="text-sm">Amount Subscribed:</span>  <span>₱ {{$record->amount_subscribed == null ? '---' : number_format($record->amount_subscribed, 2, '.', ',')}}</span></dt>
-                    <dt class="text-sm font-normal text-gray-900 space-x-3"><span class="text-sm">Initial Paid Up:</span>  <span>₱ {{$record->initial_paid_up == null ? '---' : number_format($record->initial_paid_up, 2, '.', ',')}}</span></dt>
+                    <dt class="text-sm font-normal text-gray-900 space-x-3"><span class="text-sm">Initial Paid Up:</span>  <span>₱ {{$record->initial_paid_up == null ? '---' : number_format($record->initial_paid_up, 2, '.', ',')}}</span></dt> --}}
                 </div>
-
+                <x-input label="BOD Resolution Number" disabled placeholder="---" wire:model="bod_number" />
+                <x-input label="Type / Kind of Membership" disabled placeholder="---" wire:model="membership_type" />
+                <x-input label="Number of Shares Subscribed" disabled placeholder="---" wire:model="number_of_shares" />
+                <x-input label="Amount Subscribed" disabled placeholder="---" wire:model="amount_subscribed" />
+                <x-input label="Initial Paid Up" disabled placeholder="---" wire:model="initial_paid_up" />
             </div>
-            <div class="px-4 py-6 sm:grid sm:grid-cols-5 sm:gap-4 sm:px-6">
+            {{-- <div class="px-4 py-6 sm:grid sm:grid-cols-5 sm:gap-4 sm:px-6">
 
-            </div>
+            </div> --}}
           </dl>
         </div>
       </div>
@@ -131,12 +148,13 @@
       <div class="mt-3 overflow-hidden bg-gray-50 shadow sm:rounded-lg">
         <div class="border-t border-gray-100">
           <dl class="divide-y divide-gray-100">
-            <div class="px-4 py-6 sm:grid sm:grid-cols-5 sm:gap-4 sm:px-6">
+            <div class="px-4 py-6 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-6">
                 <div class="space-y-3">
-                    <dt class="text-sm font-normal text-gray-900 space-x-3"><span class="text-sm">BOD Resolution:</span>  <span>{{$record->bod_resolution}}</span></dt>
-                    <dt class="text-sm font-normal text-gray-900 space-x-3 col-start-3"><span class="text-sm">Date:</span>  <span>{{ Carbon\Carbon::parse($record->date_created)->format('F d, Y')}}</span></dt>
+                    <x-input label="BOD Resolution" disabled placeholder="---" wire:model="bod_resolution" />
+                    {{-- <dt class="text-sm font-normal text-gray-900 space-x-3"><span class="text-sm">BOD Resolution:</span>  <span>{{$record->bod_resolution}}</span></dt>
+                    <dt class="text-sm font-normal text-gray-900 space-x-3 col-start-3"><span class="text-sm">Date:</span>  <span>{{ Carbon\Carbon::parse($record->date_created)->format('F d, Y')}}</span></dt> --}}
                 </div>
-
+                <x-input label="Date" disabled placeholder="---" wire:model="date_created" />
             </div>
           </dl>
         </div>
