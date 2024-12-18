8dc259f9584ea6845243bd8d57e4ac2851015c1a
GabJeungSook <67619600+GabJeungSook@users.noreply.github.com>
2024-12-18 20:18:06 +0800

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,7 +16,7 @@ class UserSeeder extends Seeder
     {
         User::create([
             'name' => 'Admin',
-            'email' => 'sksu@admin.com',
+            'email' => 'admin@gmail.com',
             'password' => Hash::make('password'),
         ]);
     }
